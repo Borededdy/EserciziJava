272fd5b5134b4b262318ac30d3b17ebe0e308ba5
Borededdy <83037613+Borededdy@users.noreply.github.com>
2025-05-08 17:51:19 +0200

    Aggiunto progetto Gauss

---
+++ Javamm/Lab01/Gauss/src-gen/javamm/Gauss.java
@@ -0,0 +1,10 @@
+package javamm;
+
+@SuppressWarnings("all")
+public class Gauss {
+  public static void main(String[] args) {
+    int n = 6;
+    long s = ((n * (n + 1)) / 2);
+    System.out.println(s);
+  }
+}
